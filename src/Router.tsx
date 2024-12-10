5356af3749d45beb710ba549356705ade4c2d6ee
celenibrahim <celen.ibrahim@outlook.com>
2024-12-10 19:43:02 +0300

    added server and database connection parts

--- src/Router.tsx
+++ src/Router.tsx
@@ -29,7 +29,6 @@ export default function Router() {
             component={HomeScreen}
             options={{
               headerTitleAlign: "center",
-              headerShown: false,
             }}
           />
         </Stack.Navigator>
---
+++ user-server/src/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config(); // .env dosyasını yükler
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// MongoDB bağlantısı
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB bağlantısı başarılı!");
+  } catch (error) {
+    console.error("MongoDB bağlantı hatası:", error.message);
+    process.exit(1); // Hata durumunda uygulamayı kapatır
+  }
+};
+
+// API endpoint
+app.get("/", (req, res) => {
+  res.send("API çalışıyor!");
+});
+
+// Sunucuyu başlat
+const PORT = process.env.PORT || 3000;
+
+const startServer = async () => {
+  await connectDB(); // MongoDB bağlantısını kurar
+  app.listen(PORT, () => {
+    console.log(`Sunucu ${PORT} portunda çalışıyor`);
+  });
+};
+
+startServer();
